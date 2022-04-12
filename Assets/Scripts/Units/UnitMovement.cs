b20ec948b8c2e679f184fb2d8510acc7e46ecc8b
Jeff Leu <babybluejeff@gmail.com>
2022-04-12 01:19:05 -0700

    Unit Chasing

--- Assets/Scripts/Units/UnitMovement.cs
+++ Assets/Scripts/Units/UnitMovement.cs
@@ -6,12 +6,32 @@ public class UnitMovement : NetworkBehaviour
 {
   [SerializeField] NavMeshAgent agent = null;
   [SerializeField] Targeter targeter = null;
+  [SerializeField] float chaseRange = 10f;
 
   #region Server
 
   [ServerCallback] // Prevents client from calling this
   void Update()
   {
+    Targetable target = targeter.GetTarget();
+
+    if (target != null)
+    {
+      // Optimized so we don't need to use square root which takes more to calculate
+      if ((target.transform.position - transform.position).sqrMagnitude > chaseRange * chaseRange)
+      {
+        // Chase
+        agent.SetDestination(target.transform.position);
+      }
+      else if (agent.hasPath)
+      {
+        // Stop chasing
+        agent.ResetPath();
+      }
+
+      return;
+    }
+
     // If agent doesn't have path, return so we aren't calling ResetPath, which
     // introduces a bug where sometimes agent doesn't move
     if (!agent.hasPath) { return; }
